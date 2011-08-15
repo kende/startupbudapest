593466ec06e63037dc1e4a67e080231918e1aabc
kende <kende.j@gmail.com>
2011-08-15 20:54:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budapest::Application.config.secret_token = '6b01bc60b715f24794d0a616ba35676a5badd16802464199d0247f683e8f2ae9e9ad060de5408d82d22092c16fbff40289dbf2b4ca858ebb7d37ca877f86fe78'
